1210b9a8c6cd157d1ddd4dd683feba911adbc50a
Mathew Bramson <mathewbramson@gmail.com>
2021-08-29 20:59:40 -0400

    Initial pass of the landing page

--- lib/hey_what_now_web/controllers/page_controller.ex
+++ lib/hey_what_now_web/controllers/page_controller.ex
@@ -1,7 +1,24 @@
 defmodule HeyWhatNowWeb.PageController do
   use HeyWhatNowWeb, :controller
 
-  def index(conn, _params) do
-    render(conn, "index.html")
+  def index(conn, params) do
+    changeset = form_changeset(params)
+    render(conn, "index.html", changeset: changeset)
+  end
+
+  defmodule Form do
+    @moduledoc false
+    defstruct [:activity_key]
+  end
+
+  @fields ~w(activity_key)a
+
+  defp form_changeset(attrs) do
+    struct = %Form{}
+    types = %{activity_key: :string}
+
+    {struct, types}
+    |> Ecto.Changeset.cast(attrs, @fields)
+    |> Ecto.Changeset.validate_required(@fields)
   end
 end
