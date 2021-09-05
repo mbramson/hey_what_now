aa4f065e7c47044ca381d68d5197597d082d9cd0
Mathew Bramson <mathewbramson@gmail.com>
2021-09-05 19:17:49 -0400

    Add owner when creating space while logged in

--- lib/hey_what_now_web/controllers/space_controller.ex
+++ lib/hey_what_now_web/controllers/space_controller.ex
@@ -10,10 +10,21 @@ defmodule HeyWhatNowWeb.SpaceController do
   alias HeyWhatNow.Spaces
 
   def create(conn, params) do
+    user = conn.assigns.current_user
+    params = add_current_user_to_params_as_owner(params, user)
+
     with {:ok, space} <- Spaces.create_space(params) do
       conn
       |> put_flash(:info, "Created space with key: #{space.key}")
       |> redirect(to: Routes.page_path(conn, :index))
     end
+  end
+
+  defp add_current_user_to_params_as_owner(params, nil) do
+    Map.put(params, "owner_id", nil)
+  end
+
+  defp add_current_user_to_params_as_owner(params, user) do
+    Map.put(params, "owner_id", user.id)
   end
 end
--- test/hey_what_now_web/controllers/space_controller_test.exs
+++ test/hey_what_now_web/controllers/space_controller_test.exs
@@ -1,6 +1,7 @@
 defmodule HeyWhatNowWeb.SpaceControllerTest do
-	use HeyWhatNowWeb.ConnCase, async: true
+  use HeyWhatNowWeb.ConnCase, async: true
 
+  alias HeyWhatNow.Factory
   alias HeyWhatNow.Repo
   alias HeyWhatNow.Spaces.Space
 
@@ -13,9 +14,18 @@ defmodule HeyWhatNowWeb.SpaceControllerTest do
       assert [created_space] = Repo.all(Space)
       assert created_space.name == "Untitled Space"
       refute created_space.key == nil
+      assert created_space.owner_id == nil
     end
 
-    test "if logged in, assigns the current user as owner"
+    test "does not allow specifying an arbitrary owner", %{conn: conn} do
+      user = Factory.insert(:user)
+      conn = get(conn, Routes.space_path(conn, :create, owner_id: user.id))
+      assert get_flash(conn, :info) =~ "Created space with key"
+      assert redirected_to(conn) == Routes.page_path(conn, :index)
+
+      assert [created_space] = Repo.all(Space)
+      assert created_space.owner_id == nil
+    end
 
     test "allows you to specify the name of the created space in query params", %{conn: conn} do
       get(conn, Routes.space_path(conn, :create, name: "passed_in_name"))
@@ -27,6 +37,22 @@ defmodule HeyWhatNowWeb.SpaceControllerTest do
       get(conn, Routes.space_path(conn, :create, key: "passed_in_key"))
       assert [created_space] = Repo.all(Space)
       assert created_space.key == "passed_in_key"
+    end
+  end
+
+  describe "create/2 when logged in" do
+    setup :register_and_log_in_user
+
+    test "if logged in, assigns the current user as owner", %{conn: conn, user: user} do
+      conn = get(conn, Routes.space_path(conn, :create))
+
+      assert get_flash(conn, :info) =~ "Created space with key"
+      assert redirected_to(conn) == Routes.page_path(conn, :index)
+
+      assert [created_space] = Repo.all(Space)
+      assert created_space.name == "Untitled Space"
+      refute created_space.key == nil
+      assert created_space.owner_id == user.id
     end
   end
 end
